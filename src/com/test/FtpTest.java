3200abea4db21f90f0ee9dae845ed790e7449e32
ish430 <ish430@DESKTOP-5KVJ7Q3>
2024-01-27 20:11:17 +0900

    초기 업로드

---
+++ src/com/test/FtpTest.java
@@ -0,0 +1,66 @@
+package com.test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+public class FtpTest {
+	public static void main(String[] args) {
+		FTPClient client = new FTPClient();
+		try {
+			client.connect("112.175.85.159", 21);
+			if (!client.login(getId(), getPassword())) {          // id와 password을 문자열로 작성, 예: client.login("test", "1234")
+				System.out.println("login fail.");          
+				return;        
+			}else {
+				System.out.println("login success");
+			}
+			
+			// 파일업로드
+			File newFile = new File("C:/test/newFile.txt");
+			newFile.createNewFile();
+			FileInputStream fis = new FileInputStream(newFile);
+			client.storeFile("./" + newFile.getName(), fis);
+			
+			// 파일다운로드
+			File downFile = new File("C:/test/index.html");
+			FileOutputStream fos = new FileOutputStream(downFile);
+			client.retrieveFile("./" + downFile.getName(), fos);
+			
+			// 파일목록출력
+			for (FTPFile file : client.listFiles()) {
+				System.out.println(file.getName());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				client.logout();
+				if (client.isConnected()) {
+					client.disconnect();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	private static String getPassword() {
+		return "Mnwise4210!@";
+	}
+
+	private static String getId() {
+		return "mnwiseces";
+	}
+}
---
+++ src/com/test/SftpTest.java
@@ -0,0 +1,56 @@
+package com.test;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+public class SftpTest {
+	public static void main(String[] args) {
+		JSch jsch = new JSch();
+        Session session = null;
+		try {
+			session = jsch.getSession(getId(), "112.175.85.159", 22);
+			session.setPassword(getPassword());
+	        java.util.Properties config = new java.util.Properties();
+	        config.put("StrictHostKeyChecking", "no");
+	        session.setConfig(config);
+	        session.connect();
+	        System.out.println("login success");
+	        
+	        Channel channel = session.openChannel("sftp");
+            channel.connect();
+	        ChannelSftp channelSftp = (ChannelSftp) channel;
+	        
+	        // 파일 업로드
+	        channelSftp.put("C:/test/newFile.txt", "www/test.txt");
+	        
+	        // 파일 다운로드
+	        byte[] buffer = new byte[1024];
+	        BufferedInputStream bis = new BufferedInputStream(channelSftp.get("www/index.html"));
+	        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File("C:/test/test.html")));
+			int readCount;
+			while ((readCount = bis.read(buffer)) > 0) {
+				bos.write(buffer, 0, readCount);
+			}
+			bis.close();
+	        
+	        session.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();	
+			System.out.println("login fail");
+		}
+	}
+
+	private static String getPassword() {
+		return "Mnwise4210!@";
+	}
+
+	private static String getId() {
+		return "mnwiseces";
+	}
+}
